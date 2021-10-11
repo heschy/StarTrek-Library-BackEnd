7d05e733f1dccb775d959c146bebadc911256b3f
Heschy-Software <82758794+heschy@users.noreply.github.com>
2021-10-11 20:19:24 +0200

    Commit 3
    Adding an external Database Module to clear up the code.

--- modules/starfleet.py
+++ modules/starfleet.py
@@ -1,60 +1,9 @@
 import mhn;
 from starfleet_functions import *;
+from starfleet_maindb import *;
 
 def library(name):
     output='\n';
-
-    # --- Library Building ---
-
-    # ------Library Building: Spaceships ------
-    spaceship_enterprise = [
-        'USS Enterprise (NX-01)      NX Klasse            Captain Jonathan Archer,    2151 - 2161\n',
-        'USS Enterprise (NCC-1701)   Constitution Klasse, Captain James Tiberus Kirk, 2245 - 2285\n',
-        'USS Enterprise (NCC-1701-A) Constitution Klasse, Captain James Tiberus Kirk, 2286 - 2293\n',
-        'USS Enterprise (NCC-1701-B) Excelsior Klasse,    Captain John Harriman,      2293 - Unbekannt\n',
-        'USS Enterprise (NCC-1701-C) Ambassador Klasse,   Captain Rachel Garrett,     Unbekannt - 2344\n',
-        'USS Enterprise (NCC-1701-D) Galaxy Klasse,       Captain Jean-Luc Picard,    2363 - 2371\n',
-        'USS Enterprise (NCC-1701-E) Sovereign Klasse,    Captain Jean-Luc Picard,    2372 - Unbekannt\n',
-        'USS Enterprise (NCC-1701-J) Universe Klasse,     Ubekannter Captain,         Unbekannt\n'];
-    
-    spaceship_prometheus = [
-        'USS Prometheus (NX-59650/NX-74913) Prometheus Klasse, Unbekannter Captain,            2373 - 2385\n',
-        'USS Prometheus (NCC-71201),        Nebula Klasse,     Lieutenant Commander Piersall, Unbekannt\n'  ];
-
-    spaceship_voyager = [
-        'USS Voyager (NCC-74656) Intrepid Klasse, Captain Kathryn Janeway, 2373 - 2385\n'];
-    
-    spaceship_prometheus_59650 = [
-        'USS Prometheus',
-        'Prometheus Klasse',
-        'NX-59650 und NX-74913',
-        'Unbekannt',
-        '2373 - 2385',
-        'Version 2']
-
-    starships = [
-        spaceship_enterprise,
-        spaceship_prometheus,
-        spaceship_voyager,
-        spaceship_prometheus_59650];
-
-
-    # ------ Library Builing: Starship Classes ------
-
-    starshipclass_ufp_galaxy = [
-        'Library Build uncomplete...'
-    ];
-
-
-    starshipclasses_ufp = [starshipclass_ufp_galaxy];
-    starshipclasses     = [starshipclasses_ufp];
-
-    db = [
-        starships,
-        starshipclasses
-        ];
-        
-    # --- Library Access ---
     
     if name == 'MHN' or name == 'Medizinisches Holografisches Notfallprogramm' or name == 'mhn'or name == 'Medizinisch Holografisches Notfallprogramm':
         output = mhn.v1()+'\n';
